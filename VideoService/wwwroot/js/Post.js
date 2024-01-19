4b5b93f9d9c7cacb0844c578585c57d42d66cc51
Lekaic777 <pupkov.54@inbox.ru>
2024-01-19 22:13:41 +0500

    подставил для всех комментов кнопки "Читать далее" (пока не написал скрипт, чтобы подставлять их только для больших комментов)

---
+++ VideoService/wwwroot/js/Post.js
@@ -0,0 +1,32 @@
+﻿//function ReadMoreOrLess() {
+//    var dots = document.getElementById("dots");
+//    var moreText = document.getElementById("more");
+//    var btnText = document.getElementById("readMoreBut");
+
+//    if (dots.style.display === "none") {
+//        dots.style.display = "inline";
+//        btnText.innerHTML = "Read more";
+//        moreText.style.display = "none";
+//    } else {
+//        dots.style.display = "none";
+//        btnText.innerHTML = "Read less";
+//        moreText.style.display = "inline";
+//    }
+//}
+
+function ReadMoreOrLess(id) {
+    const moreText = document.getElementById('moreText' + '(' + id + ')');
+    const but = document.getElementById('readMoreBut' + '(' + id + ')');
+    const dots = document.getElementById('dots' + '(' + id + ')')
+
+    if (moreText.style.display === 'none'
+        || moreText.style.display === '') {
+        moreText.style.display = 'inline';
+        dots.style.display = "none";
+        but.textContent = 'Read Less';
+    } else {
+        moreText.style.display = 'none';
+        but.textContent = 'Read More';
+        dots.style.display = "inline";
+    }
+} 
